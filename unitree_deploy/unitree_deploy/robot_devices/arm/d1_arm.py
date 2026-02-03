31650b6d83281231e4799521a9729f65467a8bc8
kCocokk <1271814837@qq.com>
2026-02-03 12:41:17 +0000

    修改内容说明（服务器）

--- unitree_deploy/unitree_deploy/robot_devices/arm/d1_arm.py
+++ unitree_deploy/unitree_deploy/robot_devices/arm/d1_arm.py
@@ -49,8 +49,8 @@ class D1_ArmController:
         # ===== only config params (no logic change) =====
         setattr(config, "use_relative_mapping", True)
         setattr(config, "z1_to_d1_index", [0, 1, 2, 3, 4, 5, 6])
-        # setattr(config, "d1_delta_gain", [0.65, 0.65, 0.65, 0.65, 0.65, 0.65, 1.00])
-        setattr(config, "d1_delta_gain", [1, 1, 1, 1, 1, 1, 1])
+        setattr(config, "d1_delta_gain", [0.9, 0.65, 0.65, 0.90, 0.65, 0.65, 1.00])
+        # setattr(config, "d1_delta_gain", [1, 1, 1, 1, 1, 1, 1])
         # 可选：如果你发现某些关节方向反了，再改这里的符号
         # 先给一个常见的候选（肩/肘可能方向相反），不确定就先全 1
         setattr(config, "d1_joint_sign", [1, 1, 1, 1, 1, 1, 1])
